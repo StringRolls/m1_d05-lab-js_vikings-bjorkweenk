28585c87a394e9d5bcf82ecae569b5b6827576d6
Bjork Weenk <bjorkweenk.bw@gmail.com>
2022-04-19 17:52:24 +0200

    lab4

--- src/viking.js
+++ src/viking.js
@@ -1,14 +1,65 @@
-const { Soldier, Viking, Saxon, War } = require('./../src/viking');
 // Soldier
-class Soldier {}
+class Soldier {
+  constructor(health=0, strength=0){
+    this.health = health;
+    this.strength = strength;
+  }
 
-console.log("hello world");
+  attack(){
+    return this.strength
+  }
+  
+  receiveDamage(damage){
+    this.health -= damage 
+  }
+}
 
-// Viking
-class Viking {}
+
+class Viking extends Soldier {
+  constructor(name, health, strength,){
+  super(health, strength)
+    this.name = name;
+  }
+ 
+  receiveDamage(damage){
+    super.receiveDamage(damage)
+    if( this.health > 0){
+      return `${this.name} has received ${damage} points and has ${this.health} left`;
+    } else{
+     return  `${this.name} has died in act of the combat`;
+       }
+  }
+
+  battleCry(){
+    return "Odin Owns You All!"
+  }
+}
+
+let pim = new Viking ("Pim", 9, 5);
+console.log(pim.health);
+console.log(pim.receiveDamage(10));
+console.log(pim.health);
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+constructor(health, strength){
+super(health, strength)}
+
+
+receiveDamage(damage){
+  if( this.health > 0){
+    return`A Saxon has received ${damage} points of damage`;
+  } else{
+   return `A Saxon has died in combat`;
+     }
+} 
+}
+
+let alex = new Saxon (25,28)
+console.log("Alex", alex.health);
+[0, 1, 5, 8, 11].forEach(damage=>(console.log(new Saxon(5, 28).receiveDamage(damage))));
+  //for(let damage of [0, 1, 5, 8 , 11]) alex.receiveDamage(damage)
+console.log(alex.health);
 
 // War
 class War {}
